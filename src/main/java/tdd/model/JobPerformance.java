4b9b8c99abe22097e8ceeba9ef9871de870d9c81
Bruno Dantas <brunoodantas7@gmail.com>
2022-01-06 21:21:33 -0300

    refactor finished , apply Strategy pattern and code smells better

--- src/main/java/tdd/model/JobPerformance.java
+++ src/main/java/tdd/model/JobPerformance.java
@@ -1,5 +1,27 @@
 package tdd.model;
 
+import java.math.BigDecimal;
+
 public enum JobPerformance {
-    TO_BE_DESIRED, GREAT, ABOVE_EXPECTATIONS;
+    TO_BE_DESIRED{
+        @Override
+        public BigDecimal percentReadjustment() {
+            return new BigDecimal("0.03");
+        }
+    },
+    GREAT {
+        @Override
+        public BigDecimal percentReadjustment() {
+            return new BigDecimal("0.15");
+        }
+    },
+    ABOVE_EXPECTATIONS {
+        @Override
+        public BigDecimal percentReadjustment() {
+            return new BigDecimal("0.20");
+        }
+    };
+
+    public abstract BigDecimal percentReadjustment();
+
 }
--- src/main/java/tdd/service/ReadjustmentService.java
+++ src/main/java/tdd/service/ReadjustmentService.java
@@ -9,25 +9,28 @@ public class ReadjustmentService {
 
     public void applyReadjustment(Employee employee, JobPerformance jobPerformance) {
 
-        if(jobPerformance == JobPerformance.TO_BE_DESIRED){
+        BigDecimal readjustmentValue = jobPerformance.percentReadjustment().multiply(employee.getSalario());
+        employee.readjustmentSalary(readjustmentValue);
 
-            BigDecimal readjustment = employee.getSalario().multiply(new BigDecimal("0.03"));
-
-            employee.readjustmentSalary(readjustment);
-
-        }else if (jobPerformance == JobPerformance.GREAT){
-
-            BigDecimal greatReadjustment = employee.getSalario().multiply(new BigDecimal("0.15"));
-
-            employee.readjustmentSalary(greatReadjustment);
-
-        }else{
-
-            BigDecimal aboveExpectReadjustment = employee.getSalario().multiply(new BigDecimal("0.20"));
-
-            employee.readjustmentSalary(aboveExpectReadjustment);
-
-        }
+//        if(jobPerformance == JobPerformance.TO_BE_DESIRED){
+//
+//            BigDecimal readjustment = employee.getSalario().multiply(new BigDecimal("0.03"));
+//
+//            employee.readjustmentSalary(readjustment);
+//
+//        }else if (jobPerformance == JobPerformance.GREAT){
+//
+//            BigDecimal greatReadjustment = employee.getSalario().multiply(new BigDecimal("0.15"));
+//
+//            employee.readjustmentSalary(greatReadjustment);
+//
+//        }else{
+//
+//            BigDecimal aboveExpectReadjustment = employee.getSalario().multiply(new BigDecimal("0.20"));
+//
+//            employee.readjustmentSalary(aboveExpectReadjustment);
+//
+//        }
 
     }
 }
