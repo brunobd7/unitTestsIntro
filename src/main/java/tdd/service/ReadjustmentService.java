cec27b5716b18ef4b5117e408d48f633ea5de451
Bruno Dantas <brunoodantas7@gmail.com>
2022-01-06 21:05:11 -0300

    new unit test to jobs performance , pedindg refactor to finihs TDD cicler

--- src/main/java/tdd/service/ReadjustmentService.java
+++ src/main/java/tdd/service/ReadjustmentService.java
@@ -14,6 +14,19 @@ public class ReadjustmentService {
             BigDecimal readjustment = employee.getSalario().multiply(new BigDecimal("0.03"));
 
             employee.readjustmentSalary(readjustment);
+
+        }else if (jobPerformance == JobPerformance.GREAT){
+
+            BigDecimal greatReadjustment = employee.getSalario().multiply(new BigDecimal("0.15"));
+
+            employee.readjustmentSalary(greatReadjustment);
+
+        }else{
+
+            BigDecimal aboveExpectReadjustment = employee.getSalario().multiply(new BigDecimal("0.20"));
+
+            employee.readjustmentSalary(aboveExpectReadjustment);
+
         }
 
     }
--- src/test/java/tdd/service/ReajusteServiceTest.java
+++ src/test/java/tdd/service/ReajusteServiceTest.java
@@ -11,11 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReajusteServiceTest {
 
+    //TODO IMPLEMENTAR TESTE DIRECIONADOS PELOS 2 CENARIOS FALTANTES E SEGUIR REFACTOR CONFORME AULA
 
     /** CENARIOS DE DESEMPENHO : A_DESEJAR reajuste 3% , OTIMO reajuste 15%, ACIMA_DO_ESPERADO reajuste 20% */
 
     @Test
-    public void readjustmentToBeDesired(){
+    public void readjustmentToBeDesiredIncrease3PercentOnBaseSalary(){
 
         ReadjustmentService service = new ReadjustmentService();
         Employee employee = new Employee("Bruno", LocalDate.now(),new BigDecimal("1000").setScale(2));
@@ -23,6 +24,31 @@ public class ReajusteServiceTest {
         service.applyReadjustment(employee, JobPerformance.TO_BE_DESIRED);
 
         assertEquals( new BigDecimal("1030.00"), employee.getSalario());
+
+    }
+
+    @Test
+    public void readjustmentToBeGreatIncrease15PercentOnBaseSalary(){
+
+        ReadjustmentService service = new ReadjustmentService();
+        Employee employee = new Employee("Bruno", LocalDate.now(),new BigDecimal("1000").setScale(2));
+
+        service.applyReadjustment(employee, JobPerformance.GREAT);
+
+        assertEquals( new BigDecimal("1150.00"), employee.getSalario());
+
+    }
+
+
+    @Test
+    public void readjustmentToBeAboveExpectationsIncrease20PercentOnBaseSalary(){
+
+        ReadjustmentService service = new ReadjustmentService();
+        Employee employee = new Employee("Bruno", LocalDate.now(),new BigDecimal("1000").setScale(2));
+
+        service.applyReadjustment(employee, JobPerformance.ABOVE_EXPECTATIONS);
+
+        assertEquals( new BigDecimal("1200.00"), employee.getSalario());
 
     }
 
